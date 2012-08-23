5701d892456d82d87c7286c4b00f859f7b4d99d9
chandraprakash <prakash@maidsafe.net>
2012-08-23 15:00:31 +0100

    Style fix.

--- src/maidsafe/routing/message_handler.cc
+++ src/maidsafe/routing/message_handler.cc
@@ -270,7 +270,7 @@ void MessageHandler::HandleMessage(protobuf::Message& message) {
   if (IsRelayResponseForThisNode(message))
     return HandleRoutingMessage(message);
 
-  // TODO (Team) : Needs to handle this case for low rank vaults present in Non RTs.
+  // TODO(Team) : Needs to handle this case for low rank vaults present in Non RTs.
   if (non_routing_table_.IsConnected(NodeId(message.destination_id())) &&
       IsRequest(message)) {
     LOG(kInfo) << "Dropping a request message destined to client with id: "
