9330b37ca944ea8bd7cfec74b43f54e8c864eaa4
chandraprakash <prakash@maidsafe.net>
2012-10-01 16:38:01 +0100

    Cancelling setup_timer_ on destruction.

--- src/maidsafe/routing/routing_private.cc
+++ src/maidsafe/routing/routing_private.cc
@@ -55,8 +55,9 @@ RoutingPrivate::RoutingPrivate(const asymm::Keys& keys, bool client_mode)
 }
 
 RoutingPrivate::~RoutingPrivate() {
-  recovery_timer_.cancel();
   tearing_down_ = true;
+  setup_timer_.cancel();
+  recovery_timer_.cancel();
   network_.Stop();
   boost::this_thread::disable_interruption disable_interruption;
   asio_service_.Stop();
