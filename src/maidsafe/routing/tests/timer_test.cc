b7793943b9da3d7c27cab86921fe0d01cf000257
anju.bala <anju.bala@maidsafe.net>
2013-02-06 16:16:38 +0000

    Updated test cases for Timer for changes in Timer class[#29865283]

--- src/maidsafe/routing/tests/timer_test.cc
+++ src/maidsafe/routing/tests/timer_test.cc
@@ -42,24 +42,27 @@ class TimerTest : public testing::Test {
       : asio_service_(2),
         timer_(asio_service_),
         kGroupSize_((RandomUint32() % 97) + 4),
-        single_good_response_functor_(),
-        single_failed_response_functor_(),
-        group_good_response_functor_(),
+        pass_response_functor_(),
+        failed_response_functor_(),
         group_failed_response_functor_(),
-        message_() {
+        message_(),
+        pass_response_count_(0),
+        failed_response_count_(0) {
     asio_service_.Start();
 
-    single_good_response_functor_ = [=](std::vector<std::string> response) {
-      ASSERT_EQ(1U, response.size());
+    pass_response_functor_ = [=](std::string response) {
+      ++pass_response_count_;
+      ASSERT_FALSE(response.empty());
     };
-    single_failed_response_functor_ = [=](std::vector<std::string> response) {
+    failed_response_functor_ = [=](std::string response) {
       ASSERT_TRUE(response.empty());
+      ++failed_response_count_;
     };
-    group_good_response_functor_ = [=](std::vector<std::string> response) {
-      ASSERT_EQ(kGroupSize_, response.size());
-    };
-    group_failed_response_functor_ = [=](std::vector<std::string> response) {
-      ASSERT_EQ(kGroupSize_ - 1, response.size());
+    group_failed_response_functor_ = [=](std::string response) {
+      if (response.empty())
+        ++failed_response_count_;
+      else
+        ++pass_response_count_;
     };
 
     message_.set_type(-200);
@@ -69,19 +72,7 @@ class TimerTest : public testing::Test {
     message_.set_source_id("source_id");
   }
 
-  void CheckResponse(std::vector<std::future<std::string>> &future_in,
-                      const uint32_t& response_size,
-                      const uint32_t& exceptions) {
-    uint32_t count = 0;
-    for (; count < response_size; ++count)
-      future_in.at(count).get();
-    ASSERT_EQ(count, response_size);
-    for (count = 0; count < exceptions; ++count) {
-      EXPECT_THROW(future_in.at(response_size + count).get(), std::exception);
-    }
-  }
-
-  protobuf::Message  CreateMessage(const int id) {
+    protobuf::Message  CreateMessage(const int id) {
     protobuf::Message message;
     message.set_type(-200);
     message.set_destination_id("destination_id");
@@ -96,39 +87,27 @@ class TimerTest : public testing::Test {
   AsioService asio_service_;
   Timer timer_;
   const uint16_t kGroupSize_;
-  TaskResponseFunctor single_good_response_functor_,
-      single_failed_response_functor_;
-  TaskResponseFunctor group_good_response_functor_,
-      group_failed_response_functor_;
+  TaskResponseFunctor pass_response_functor_, failed_response_functor_;
+  TaskResponseFunctor group_failed_response_functor_;
   protobuf::Message message_;
+  uint16_t pass_response_count_;
+  uint16_t failed_response_count_;
 };
 
 TEST_F(TimerTest, BEH_SingleResponse) {
   message_.set_id(timer_.AddTask(bptime::seconds(2),
-                                 single_good_response_functor_, 1));
+                                 pass_response_functor_, 1));
   timer_.AddResponse(message_);
-}
-
-TEST_F(TimerTest, BEH_Promise_SingleResponse) {
-  uint32_t response_size = 1;
-  std::vector<std::shared_ptr<std::promise<std::string>>> promises_in;
-  std::vector<std::future<std::string>> future_in;
-  for (uint32_t count = 0; count < response_size; ++count) {
-    promises_in.push_back(std::make_shared<std::promise<std::string>>());
-    future_in.push_back(promises_in[count]->get_future());
-  }
-  message_.set_id(timer_.AddTask(bptime::seconds(2), promises_in));
-  timer_.AddResponse(message_);
-  CheckResponse(future_in, response_size, 0);
+  while (pass_response_count_ != 1U);
 }
 
 TEST_F(TimerTest, BEH_MultipleResponse) {
-  const int kMessageCount(400);
+  const int kMessageCount(300);
   boost::progress_timer t;
   std::atomic<int> count(0);
-  TaskResponseFunctor response_functor = [&count](std::vector<std::string> response) {
-    ASSERT_EQ(1U, response.size());
+  TaskResponseFunctor response_functor = [&count](std::string response) {
     ++count;
+    EXPECT_FALSE(response.empty());
   };
 
   auto add_tasks = [&](const int& number)->std::vector<protobuf::Message> {
@@ -158,11 +137,11 @@ TEST_F(TimerTest, BEH_MultipleResponse) {
 }
 
 TEST_F(TimerTest, BEH_MultipleGroupResponse) {
-  const int kMessageCount(400);
+  const int kMessageCount(300);
   boost::progress_timer t;
   std::atomic<int> count(0);
-  TaskResponseFunctor response_functor = [&count](std::vector<std::string> response) {
-    ASSERT_EQ(4U, response.size());
+  TaskResponseFunctor response_functor = [&count](std::string response) {
+    EXPECT_FALSE(response.empty());
     ++count;
   };
 
@@ -191,180 +170,24 @@ TEST_F(TimerTest, BEH_MultipleGroupResponse) {
   add_response1_future.get();
   add_response2_future.get();
 
-  while (count != (kMessageCount));
-}
-
-TEST_F(TimerTest, BEH_Promise_MultipleResponse) {
-  const int kMessageCount(400);
-  boost::progress_timer t;
-  std::atomic<int> count(0);
-  std::shared_ptr<std::vector<std::future<std::string>>>
-     futures1 = std::make_shared<std::vector<std::future<std::string>>>();
-  std::shared_ptr<std::vector<std::future<std::string>>>
-     futures2 = std::make_shared<std::vector<std::future<std::string>>>();
-
-  auto add_tasks = [&](const int& number,
-      std::shared_ptr<std::vector<std::future<std::string>>>
-                       futures)->std::vector<protobuf::Message> {
-        std::vector<protobuf::Message> messages;
-        for (int i(0); i != number; ++i) {
-          std::vector<std::shared_ptr<std::promise<std::string>>> promises_in;
-          promises_in.push_back(std::make_shared<std::promise<std::string>>());
-          auto future(promises_in[0]->get_future());
-          futures->push_back(std::move(future));
-          messages.push_back(std::move(CreateMessage(timer_.AddTask(bptime::seconds(50),
-                                                     promises_in))));
-        }
-        return messages;
-      };
-
-  auto add_response = [&](std::vector<protobuf::Message>&& messages) {
-        for (size_t i(0); i != messages.size(); ++i)
-          timer_.AddResponse(messages.at(i));
-      };
-
-  auto add_tasks1_future = std::async(std::launch::async, add_tasks, kMessageCount / 2, futures1);
-  auto add_tasks2_future = std::async(std::launch::async, add_tasks, kMessageCount / 2, futures2);
-
-  std::vector<protobuf::Message> messages1(std::move(add_tasks1_future.get()));
-  std::vector<protobuf::Message> messages2(std::move(add_tasks2_future.get()));
-
-  auto add_response1_future = std::async(std::launch::async, add_response, std::move(messages1));
-  auto add_response2_future = std::async(std::launch::async, add_response, std::move(messages2));
-  add_response1_future.get();
-  add_response2_future.get();
-
-  for (auto itr(futures2->begin()); itr != futures2->end(); ++itr)
-    futures1->push_back(std::move(*itr));
-
-  while (!futures1->empty()) {
-    futures1->erase(std::remove_if(futures1->begin(), futures1->end(),
-        [&count](std::future<std::string>& str)->bool {
-            if (IsReady(str)) {
-              try {
-                auto response(str.get());
-                EXPECT_FALSE(response.empty());
-                ++count;
-              } catch(std::exception& ex) {
-                LOG(kError) << "Exception : " << ex.what();
-                EXPECT_TRUE(false) << ex.what();
-              }
-                return true;
-              } else  {
-                return false;
-              };
-        }), futures1->end());
-    std::this_thread::yield();
-  }
-  ASSERT_EQ(kMessageCount, count);
-}
-
-TEST_F(TimerTest, BEH_Promise_MultipleGroupResponse) {
-  const int kMessageCount(400);
-  boost::progress_timer t;
-  std::atomic<int> count(0);
-  std::shared_ptr<std::vector<std::future<std::string>>>
-     futures1 = std::make_shared<std::vector<std::future<std::string>>>();
-  std::shared_ptr<std::vector<std::future<std::string>>>
-     futures2 = std::make_shared<std::vector<std::future<std::string>>>();
-  auto add_tasks = [&](const int& number,
-      std::shared_ptr<std::vector<std::future<std::string>>>
-                       futures)->std::vector<protobuf::Message> {
-      std::vector<protobuf::Message> messages;
-      for (int i(0); i != number; ++i) {
-        std::vector<std::shared_ptr<std::promise<std::string>>> promises_in;
-        for (size_t j(0); j != 4; ++j) {
-          promises_in.push_back(std::make_shared<std::promise<std::string>>());
-          auto future(promises_in[j]->get_future());
-          futures->push_back(std::move(future));
-        }
-        messages.push_back(std::move(CreateMessage(timer_.AddTask(bptime::seconds(50),
-                                                   promises_in))));
-      }
-      return messages;
-    };
-
-  auto add_response = [&](std::vector<protobuf::Message>&& messages) {
-      for (size_t i(0); i != messages.size(); ++i)
-        for (size_t j(0); j != 4; ++j)
-          timer_.AddResponse(messages.at(i));
-    };
-
-  auto add_tasks1_future = std::async(std::launch::async, add_tasks, kMessageCount / 2, futures1);
-  auto add_tasks2_future = std::async(std::launch::async, add_tasks, kMessageCount / 2, futures2);
-
-  std::vector<protobuf::Message> messages1(std::move(add_tasks1_future.get()));
-  std::vector<protobuf::Message> messages2(std::move(add_tasks2_future.get()));
-
-  auto add_response1_future = std::async(std::launch::async, add_response, std::move(messages1));
-  auto add_response2_future = std::async(std::launch::async, add_response, std::move(messages2));
-  add_response1_future.get();
-  add_response2_future.get();
-
-  for (auto itr(futures2->begin()); itr != futures2->end(); ++itr)
-    futures1->push_back(std::move(*itr));
-
-  while (!futures1->empty()) {
-    futures1->erase(std::remove_if(futures1->begin(), futures1->end(),
-        [&count](std::future<std::string>& str)->bool {
-            if (IsReady(str)) {
-              try {
-                auto response(str.get());
-                EXPECT_FALSE(response.empty());
-                ++count;
-              } catch(std::exception& ex) {
-                LOG(kError) << "Exception : " << ex.what();
-                EXPECT_TRUE(false) << ex.what();
-              }
-                return true;
-              } else  {
-                return false;
-              };
-        }), futures1->end());
-    std::this_thread::yield();
-  }
-  ASSERT_EQ((kMessageCount * 4), count);
-}
-
-TEST_F(TimerTest, BEH_Promise_SingleResponseTimedOut) {
-  uint32_t response_size = 1;
-  std::vector<std::shared_ptr<std::promise<std::string>>> promises_in;
-  std::vector<std::future<std::string>> future_in;
-  for (uint32_t count = 0; count < response_size; ++count) {
-    promises_in.push_back(std::make_shared<std::promise<std::string>>());
-  }
-  timer_.AddTask(bptime::milliseconds(100), promises_in);
-  Sleep(bptime::milliseconds(200));
-  CheckResponse(future_in, 0, 1);
+  while (count != (kMessageCount * 4));
 }
 
 TEST_F(TimerTest, BEH_SingleResponseTimedOut) {
-  timer_.AddTask(bptime::milliseconds(100), single_failed_response_functor_,
+  timer_.AddTask(bptime::milliseconds(100), failed_response_functor_,
                  1);
   boost::this_thread::disable_interruption disable_interruption;
   Sleep(bptime::milliseconds(200));
+  EXPECT_EQ(failed_response_count_, 1U);
 }
 
 TEST_F(TimerTest, BEH_GroupResponse) {
   message_.set_id(timer_.AddTask(bptime::seconds(2),
-                                 group_good_response_functor_,
+                                 pass_response_functor_,
                                  kGroupSize_));
   for (uint16_t i(0); i != kGroupSize_; ++i)
     timer_.AddResponse(message_);
-}
-
-TEST_F(TimerTest, BEH_Promise_GroupResponse) {
-  uint32_t response_size = kGroupSize_;
-  std::vector<std::shared_ptr<std::promise<std::string>>> promises_in;
-  std::vector<std::future<std::string>> future_in;
-  for (uint32_t count = 0; count < response_size; ++count) {
-    promises_in.push_back(std::make_shared<std::promise<std::string>>());
-    future_in.push_back(promises_in[count]->get_future());
-  }
-  message_.set_id(timer_.AddTask(bptime::seconds(2), promises_in));
-  for (uint32_t count = 0; count < response_size; ++count)
-    timer_.AddResponse(message_);
-  CheckResponse(future_in, response_size, 0);
+  while (pass_response_count_ != kGroupSize_);
 }
 
 TEST_F(TimerTest, BEH_GroupResponsePartialResult) {
@@ -375,23 +198,10 @@ TEST_F(TimerTest, BEH_GroupResponsePartialResult) {
     timer_.AddResponse(message_);
 
   Sleep(bptime::milliseconds(500));
-}
+  while ((pass_response_count_ + failed_response_count_) != kGroupSize_);
 
-TEST_F(TimerTest, BEH_Promise_GroupResponsePartialResult) {
-  uint32_t response_size = kGroupSize_;
-  std::vector<std::shared_ptr<std::promise<std::string>>> promises_in;
-  std::vector<std::future<std::string>> future_in;
-  for (uint32_t count = 0; count < response_size -1; ++count) {
-    promises_in.push_back(std::make_shared<std::promise<std::string>>());
-    future_in.push_back(promises_in[count]->get_future());
-  }
-  message_.set_id(timer_.AddTask(bptime::milliseconds(200), promises_in));
-  for (uint32_t count = 0; count < response_size - 1; ++count)
-    timer_.AddResponse(message_);
-
-  Sleep(bptime::milliseconds(500));
-
-  CheckResponse(future_in, response_size - 1, 1);
+  EXPECT_EQ(pass_response_count_, (kGroupSize_ - 1));
+  EXPECT_EQ(failed_response_count_, 1);
 }
 
 TEST_F(TimerTest, BEH_VariousResults) {
@@ -400,13 +210,13 @@ TEST_F(TimerTest, BEH_VariousResults) {
   for (int i(0); i != 100; ++i) {
     // Single message with response
     message_.set_id(timer_.AddTask(bptime::seconds(10),
-                                   single_good_response_functor_, 1));
+                                   pass_response_functor_, 1));
     messages_to_be_added.push_back(message_);
     // Single message without response
-    timer_.AddTask(bptime::seconds(5), single_failed_response_functor_, 1);
+    timer_.AddTask(bptime::seconds(5), failed_response_functor_, 1);
     // Group message with all responses
     message_.set_id(timer_.AddTask(bptime::seconds(10),
-                                   group_good_response_functor_,
+                                   pass_response_functor_,
                                    kGroupSize_));
     for (uint16_t i(0); i != kGroupSize_; ++i)
       messages_to_be_added.push_back(message_);
@@ -426,56 +236,8 @@ TEST_F(TimerTest, BEH_VariousResults) {
 
   boost::this_thread::disable_interruption disable_interruption;
   Sleep(bptime::seconds(5));
-}
-
-TEST_F(TimerTest, BEH_Promise_VariousResults) {
-  uint32_t response_size = kGroupSize_;
-  std::vector<std::shared_ptr<std::promise<std::string>>> single_promises_in;
-  std::vector<std::shared_ptr<std::promise<std::string>>> group_promises_in;
-  std::vector<std::shared_ptr<std::promise<std::string>>> partial_promises_in;
-  std::vector<std::shared_ptr<std::promise<std::string>>> failed_promises_in;
-
-  std::vector<std::future<std::string>> single_future_in;
-  std::vector<std::future<std::string>> group_future_in;
-  std::vector<std::future<std::string>> partial_future_in;
-  std::vector<std::future<std::string>> failed_future_in;
-
-  single_promises_in.push_back(std::make_shared<std::promise<std::string>>());
-  single_future_in.push_back(single_promises_in[0]->get_future());
-
-  for (uint32_t count = 0; count < response_size; ++count) {
-    group_promises_in.push_back(std::make_shared<std::promise<std::string>>());
-    group_future_in.push_back(group_promises_in[count]->get_future());
-  }
-  for (uint32_t count = 0; count < response_size - 1; ++count) {
-    partial_promises_in.push_back(std::make_shared<std::promise<std::string>>());
-    partial_future_in.push_back(partial_promises_in[count]->get_future());
-  }
-  std::vector<protobuf::Message> messages_to_be_added;
-  messages_to_be_added.reserve(kGroupSize_ * 2);
-  // Single message with response
-  message_.set_id(timer_.AddTask(bptime::seconds(10), single_promises_in));
-  messages_to_be_added.push_back(message_);
-  // Single message without response
-  timer_.AddTask(bptime::seconds(5), failed_promises_in);
-  // Group message with all responses
-  message_.set_id(timer_.AddTask(bptime::seconds(10), group_promises_in));
-  for (uint16_t i(0); i != kGroupSize_; ++i)
-  messages_to_be_added.push_back(message_);
-  // Group message with all bar one responses
-  message_.set_id(timer_.AddTask(bptime::seconds(5), partial_promises_in));
-  for (uint16_t i(0); i != kGroupSize_ - 1; ++i)
-    messages_to_be_added.push_back(message_);
-
-  std::random_shuffle(messages_to_be_added.begin(), messages_to_be_added.end());
-
-  for (const protobuf::Message& message : messages_to_be_added)
-    timer_.AddResponse(message);
-
-  Sleep(bptime::seconds(5));
-  CheckResponse(single_future_in, 1, 0);
-  CheckResponse(group_future_in, response_size, 0);
-  CheckResponse(partial_future_in, response_size - 1, 1);
+  EXPECT_EQ(pass_response_count_, (100 + (100 * kGroupSize_) + (100 * (kGroupSize_ - 1))));
+  EXPECT_EQ(failed_response_count_, 100 + 100);
 }
 
 }  // namespace test
