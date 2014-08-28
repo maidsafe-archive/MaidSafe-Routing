e637a095fb1297454fa11e0ab063033b0b39549d
moadeli <mmoadeli@maidsafe.net>
2014-08-28 12:38:26 +0100

    An attempt to fix FUNC_NonMutatingClientSendToGroupRandomId by increasing timeout

--- src/maidsafe/routing/tests/routing_functional_test.cc
+++ src/maidsafe/routing/tests/routing_functional_test.cc
@@ -191,6 +191,8 @@ TEST_F(RoutingNetworkTest, FUNC_SendToGroupSelfId) {
   env_->ClearMessages();
   std::vector<std::future<std::unique_ptr<testing::AssertionResult>>> futures;
 
+  auto timeout(Parameters::default_response_timeout);
+  Parameters::default_response_timeout *= message_count;
   for (unsigned int dest_index(0); dest_index < kServerSize; ++dest_index) {
     NodeId dest_id(env_->nodes_.at(dest_index)->node_id());
     futures.emplace_back(
@@ -204,7 +206,7 @@ TEST_F(RoutingNetworkTest, FUNC_SendToGroupSelfId) {
     futures.erase(
         std::remove_if(
             futures.begin(), futures.end(),
-            [](std::future<std::unique_ptr<testing::AssertionResult>> & future_bool)->bool {
+            [](std::future<std::unique_ptr<testing::AssertionResult>>& future_bool)->bool {
               if (IsReady(future_bool)) {
                 EXPECT_TRUE(*future_bool.get());
                 return true;
@@ -222,6 +224,7 @@ TEST_F(RoutingNetworkTest, FUNC_SendToGroupSelfId) {
   }
   EXPECT_EQ(message_count * (Parameters::group_size) * kServerSize, receivers_message_count);
   LOG(kVerbose) << "Total message received count : " << receivers_message_count;
+  Parameters::default_response_timeout = timeout;
 }
 
 TEST_F(RoutingNetworkTest, FUNC_SendToGroupClientSelfId) {
@@ -266,6 +269,8 @@ TEST_F(RoutingNetworkTest, FUNC_SendToGroupRandomId) {
   env_->ClearMessages();
   std::vector<std::future<std::unique_ptr<testing::AssertionResult>>> futures;
 
+  auto timeout(Parameters::default_response_timeout);
+  Parameters::default_response_timeout *= message_count;
   for (unsigned int index = 0; index < message_count; ++index) {
     futures.emplace_back(std::async(std::launch::async, [this]() {
       return std::move(std::unique_ptr<testing::AssertionResult>(
@@ -277,7 +282,7 @@ TEST_F(RoutingNetworkTest, FUNC_SendToGroupRandomId) {
     futures.erase(
         std::remove_if(
             futures.begin(), futures.end(),
-            [](std::future<std::unique_ptr<testing::AssertionResult>> & future_bool)->bool {
+            [](std::future<std::unique_ptr<testing::AssertionResult>>& future_bool)->bool {
               if (IsReady(future_bool)) {
                 EXPECT_TRUE(*future_bool.get());
                 return true;
@@ -294,6 +299,7 @@ TEST_F(RoutingNetworkTest, FUNC_SendToGroupRandomId) {
   }
   EXPECT_EQ(message_count * (Parameters::group_size), receivers_message_count);
   LOG(kVerbose) << "Total message received count : " << message_count * (Parameters::group_size);
+  Parameters::default_response_timeout = timeout;
 }
 
 TEST_F(RoutingNetworkTest, FUNC_NonMutatingClientSendToGroupRandomId) {
@@ -301,6 +307,8 @@ TEST_F(RoutingNetworkTest, FUNC_NonMutatingClientSendToGroupRandomId) {
   env_->ClearMessages();
   std::vector<std::future<std::unique_ptr<testing::AssertionResult>>> futures;
 
+  auto timeout(Parameters::default_response_timeout);
+  Parameters::default_response_timeout *= message_count;
   env_->AddMutatingClient(false);
   assert(env_->nodes_.size() - 1 < std::numeric_limits<unsigned int>::max());
 
@@ -316,7 +324,7 @@ TEST_F(RoutingNetworkTest, FUNC_NonMutatingClientSendToGroupRandomId) {
     futures.erase(
         std::remove_if(
             futures.begin(), futures.end(),
-            [](std::future<std::unique_ptr<testing::AssertionResult>> & future_bool)->bool {
+            [](std::future<std::unique_ptr<testing::AssertionResult>>& future_bool)->bool {
               if (IsReady(future_bool)) {
                 EXPECT_TRUE(*future_bool.get());
                 return true;
@@ -335,6 +343,7 @@ TEST_F(RoutingNetworkTest, FUNC_NonMutatingClientSendToGroupRandomId) {
 
   EXPECT_EQ(message_count * (Parameters::group_size), receivers_message_count);
   LOG(kVerbose) << "Total message received count : " << message_count * (Parameters::group_size);
+  Parameters::default_response_timeout = timeout;
 }
 
 TEST_F(RoutingNetworkTest, FUNC_NonMutatingClientSendToGroupExistingId) {
