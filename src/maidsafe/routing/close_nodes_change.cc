c6613b808c34e15331b576bb8c4756310d661405
mmoadeli <mmoadeli@maidsafe.net>
2015-01-19 22:42:12 +0000

    further work on re-factoring close nodes change

--- src/maidsafe/routing/close_nodes_change.cc
+++ src/maidsafe/routing/close_nodes_change.cc
@@ -57,23 +57,25 @@ ConnectionsChange::ConnectionsChange(NodeId this_node_id,
     : node_id_(std::move(this_node_id)),
       lost_node_([&, this]() -> NodeId {
         std::vector<NodeId> lost_nodes;
-        std::set_difference(std::begin(old_close_nodes), std::end(old_close_nodes),
-                            std::begin(new_close_nodes), std::end(new_close_nodes),
-                            std::back_inserter(lost_nodes),
-                            [this](const NodeId& lhs, const NodeId& rhs) {
-          return NodeId::CloserToTarget(lhs, rhs, node_id_);
-        });
+        for (const auto old_node : old_close_nodes) {
+          if (std::none_of(new_close_nodes.begin(), new_close_nodes.end(),
+                           [&old_node](const NodeId& node_id){
+                             return node_id == old_node;
+                           }))
+            lost_nodes.emplace_back(old_node);
+        }
         assert(lost_nodes.size() <= 1);
         return (lost_nodes.empty()) ? NodeId() : lost_nodes.at(0);
       }()),
       new_node_([&, this]() -> NodeId {
         std::vector<NodeId> new_nodes;
-        std::set_difference(std::begin(new_close_nodes), std::end(new_close_nodes),
-                            std::begin(old_close_nodes), std::end(old_close_nodes),
-                            std::back_inserter(new_nodes),
-                            [this](const NodeId& lhs, const NodeId& rhs) {
-          return NodeId::CloserToTarget(lhs, rhs, node_id_);
-        });
+        for (const auto new_node : new_close_nodes) {
+        if (std::none_of(old_close_nodes.begin(), old_close_nodes.end(),
+                         [&new_node](const NodeId& node_id){
+                           return node_id == new_node;
+                         }))
+          new_nodes.emplace_back(new_node);
+        }
         assert(new_nodes.size() <= 1);
         return (new_nodes.empty()) ? NodeId() : new_nodes.at(0);
       }()) {}
@@ -92,14 +94,17 @@ void swap(ConnectionsChange& lhs, ConnectionsChange& rhs) MAIDSAFE_NOEXCEPT {
   swap(lhs.new_node_, rhs.new_node_);
 }
 
+// ============================== client node change =========================================
+
 ClientNodesChange::ClientNodesChange() : ConnectionsChange() {}
+
 ClientNodesChange::ClientNodesChange(const ClientNodesChange& other)
     : ConnectionsChange(other) {}
 
 ClientNodesChange::ClientNodesChange(ClientNodesChange&& other)
     : ConnectionsChange(std::move(other)) {}
 
-ClientNodesChange::ClientNodesChange& operator=(ClientNodesChange other) {
+ClientNodesChange& ClientNodesChange::operator=(ClientNodesChange other) {
   std::swap(*this, other);
   return *this;
 }
@@ -110,12 +115,64 @@ ClientNodesChange::ClientNodesChange(NodeId this_node_id,
     : ConnectionsChange(this_node_id, old_close_nodes, new_close_nodes) {}
 
 std::string ClientNodesChange::ReportConnection() const {
-  return ConnectionsChange::Print();
+  std::stringstream stringstream;
+  {
+    cereal::JSONOutputArchive archive{stringstream};
+    if (lost_node().IsValid())
+      archive(cereal::make_nvp("clientRemoved",
+                               lost_node().ToStringEncoded(NodeId::EncodingType::kHex)));
+    if (new_node().IsValid())
+      archive(cereal::make_nvp("clientAdded",
+                               new_node().ToStringEncoded(NodeId::EncodingType::kHex)));
+  }
+  return stringstream.str();
 }
 
 std::string ClientNodesChange::Print() const {
   return ConnectionsChange::Print();
 }
+
+void swap(ClientNodesChange& lhs, ClientNodesChange& rhs) MAIDSAFE_NOEXCEPT {
+  using std::swap;
+  swap(dynamic_cast<ConnectionsChange&>(lhs), dynamic_cast<ConnectionsChange&>(rhs));
+}
+
+// ========================== non-client client close nodes change =================================
+
+CloseNodesChange::CloseNodesChange() : ConnectionsChange() {}
+
+CloseNodesChange::CloseNodesChange(const CloseNodesChange& other)
+    : ConnectionsChange(other), old_close_nodes_(other.old_close_nodes_),
+      new_close_nodes_(other.old_close_nodes_), radius_(other.radius_) {}
+
+CloseNodesChange::CloseNodesChange(CloseNodesChange&& other)
+  : ConnectionsChange(std::move(other)), old_close_nodes_(std::move(other.old_close_nodes_)),
+  new_close_nodes_(std::move(other.old_close_nodes_)), radius_(std::move(other.radius_)) {}
+
+CloseNodesChange& CloseNodesChange::operator=(CloseNodesChange other) {
+  std::swap(*this, other);
+  return *this;
+}
+
+CloseNodesChange::CloseNodesChange(NodeId this_node_id,
+                                   const std::vector<NodeId>& old_close_nodes,
+                                   const std::vector<NodeId>& new_close_nodes)
+    : ConnectionsChange(this_node_id, old_close_nodes, new_close_nodes),
+      old_close_nodes_([this](std::vector<NodeId> old_close_nodes_in) -> std::vector<NodeId> {
+        std::sort(std::begin(old_close_nodes_in), std::end(old_close_nodes_in),
+                  [this](const NodeId& lhs, const NodeId& rhs) {
+                    return NodeId::CloserToTarget(lhs, rhs, node_id());
+                  });
+        return old_close_nodes_in;
+      }(old_close_nodes)),
+      new_close_nodes_([this](std::vector<NodeId> new_close_nodes_in) -> std::vector<NodeId> {
+        std::sort(std::begin(new_close_nodes_in), std::end(new_close_nodes_in),
+                  [this](const NodeId& lhs, const NodeId& rhs) {
+                    return NodeId::CloserToTarget(lhs, rhs, node_id());
+                  });
+        return new_close_nodes_in;
+      }(new_close_nodes)) {}
+
 
 std::string CloseNodesChange::ReportConnection() const {
   std::stringstream stringstream;
@@ -152,6 +209,115 @@ std::string CloseNodesChange::ReportConnection() const {
     }
   }
   return stringstream.str();
+}
+
+CheckHoldersResult CloseNodesChange::CheckHolders(const NodeId& target) const {
+  // Handle cases of lower number of group close_nodes nodes
+  size_t group_size_adjust(Parameters::group_size + 1U);
+  size_t old_holders_size = std::min(old_close_nodes_.size(), group_size_adjust);
+  size_t new_holders_size = std::min(new_close_nodes_.size(), group_size_adjust);
+  
+  std::vector<NodeId> old_holders(old_holders_size), new_holders(new_holders_size);
+  
+  std::partial_sort_copy(std::begin(old_close_nodes_), std::end(old_close_nodes_),
+                         std::begin(old_holders), std::end(old_holders),
+                         [target](const NodeId& lhs, const NodeId& rhs) {
+                           return NodeId::CloserToTarget(lhs, rhs, target);
+                         });
+  std::partial_sort_copy(std::begin(new_close_nodes_), std::end(new_close_nodes_),
+                         std::begin(new_holders), std::end(new_holders),
+                         [target](const NodeId& lhs, const NodeId& rhs) {
+                           return NodeId::CloserToTarget(lhs, rhs, target);
+                         });
+  
+  // Remove target == node ids and adjust holder size
+  old_holders.erase(std::remove(std::begin(old_holders), std::end(old_holders), target),
+                    std::end(old_holders));
+  if (old_holders.size() > Parameters::group_size) {
+    old_holders.resize(Parameters::group_size);
+    assert(old_holders.size() == Parameters::group_size);
+  }
+  
+  new_holders.erase(std::remove(std::begin(new_holders), std::end(new_holders), target),
+                    std::end(new_holders));
+  if (new_holders.size() > Parameters::group_size) {
+    new_holders.resize(Parameters::group_size);
+    assert(new_holders.size() == Parameters::group_size);
+  }
+  
+  CheckHoldersResult holders_result;
+  holders_result.proximity_status = GroupRangeStatus::kOutwithRange;
+  if (!new_holders.empty() && ((new_holders.size() < Parameters::group_size) ||
+                               NodeId::CloserToTarget(node_id(), new_holders.back(), target))) {
+    holders_result.proximity_status = GroupRangeStatus::kInRange;
+    if (new_holders.size() == Parameters::group_size)
+      new_holders.pop_back();
+    new_holders.push_back(node_id());
+  }
+  
+  if (!old_holders.empty() && NodeId::CloserToTarget(node_id(), old_holders.back(), target)) {
+    old_holders.pop_back();
+    if (old_holders.size() == Parameters::group_size)
+      old_holders.pop_back();
+    old_holders.push_back(node_id());
+  }
+  
+  std::vector<NodeId> diff_new_holders;
+  std::for_each(std::begin(new_holders), std::end(new_holders), [&](const NodeId& new_holder) {
+    if (std::find(std::begin(old_holders), std::end(old_holders), new_holder) ==
+        std::end(old_holders))
+      diff_new_holders.push_back(new_holder);
+  });
+  //   holders_result.new_holders = new_holders;
+  //   holders_result.old_holders = old_holders;
+  if (diff_new_holders.size() > 0)
+    holders_result.new_holder = diff_new_holders.front();
+  // in case the new_holder is the node itself, it shall be ignored
+  if (holders_result.new_holder == node_id())
+    holders_result.new_holder = NodeId();
+  return holders_result;
+}
+
+bool CloseNodesChange::CheckIsHolder(const NodeId& target, const NodeId& node_id) const {
+  if (new_close_nodes_.size() < Parameters::group_size)
+    return true;
+  
+  std::vector<NodeId> holders(Parameters::group_size);
+  std::partial_sort_copy(std::begin(new_close_nodes_), std::end(new_close_nodes_),
+                         std::begin(holders), std::end(holders),
+                         [target](const NodeId& lhs, const NodeId& rhs) {
+                           return NodeId::CloserToTarget(lhs, rhs, target);
+                         });
+  
+  return (std::find(std::begin(holders), std::end(holders), node_id) != std::end(holders));
+}
+
+NodeId CloseNodesChange::ChoosePmidNode(const std::set<NodeId>& online_pmids,
+                                        const NodeId& target) const {
+  if (online_pmids.empty())
+    BOOST_THROW_EXCEPTION(MakeError(CommonErrors::invalid_parameter));
+  
+  // In case storing to PublicPmid, the data shall not be stored on the Vault itself
+  // However, the vault will appear in DM's routing table and affect result
+  std::vector<NodeId> temp(Parameters::group_size + 1);
+  std::partial_sort_copy(std::begin(new_close_nodes_), std::end(new_close_nodes_), std::begin(temp),
+                         std::end(temp), [&target](const NodeId& lhs, const NodeId& rhs) {
+                           return NodeId::CloserToTarget(lhs, rhs, target);
+                         });
+  
+  auto temp_itr(std::begin(temp));
+  auto pmids_itr(std::begin(online_pmids));
+  while (*temp_itr != node_id()) {
+    ++temp_itr;
+    //     assert(temp_itr != std::end(temp));
+    if (temp_itr == std::end(temp)) {
+      LOG(kError) << "node_id_ not listed in group range having " << temp.size() << " nodes";
+      break;
+    }
+    if (++pmids_itr == std::end(online_pmids))
+      pmids_itr = std::begin(online_pmids);
+  }
+  return *pmids_itr;
 }
 
 void swap(CloseNodesChange& lhs, CloseNodesChange& rhs) MAIDSAFE_NOEXCEPT {
