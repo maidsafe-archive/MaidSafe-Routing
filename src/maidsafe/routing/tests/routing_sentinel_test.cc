7eaf998a4aa7e83bb29dddbad35332d772e2852e
Benjamin Bollen <benjamin.bollen@maidsafe.net>
2015-03-09 15:12:23 +0000

    Starting to take some shape

--- src/maidsafe/routing/tests/routing_sentinel_test.cc
+++ src/maidsafe/routing/tests/routing_sentinel_test.cc
@@ -40,26 +40,37 @@ namespace test {
 // structure to track messages sent to sentinel and what sentinel result should be
 class SignatureGroup {
  public:
-  SignatureGroup(GroupAddress group_address, size_t group_size, size_t active_quorum);
+  SignatureGroup(GroupAddress group_address, size_t group_size, size_t active_quorum)
+      : group_address_(std::move(group_address)),
+        group_size_(std::move(group_size)),
+        active_quorum_(std::move(active_quorum)) {
+    for (size_t i = 0; i < group_size; i++) {
+      nodes_.push_back(passport::CreatePmidAndSigner().first);
+    }
+  }
 
   GroupAddress Address() { return group_address_; }
 
  private:
   GroupAddress group_address_;
-  std::vector<std::pair<Identity, passport::PublicPmid>> nodes_;
+  size_t group_size_;
+  size_t active_quorum_;
+  std::vector<passport::Pmid> nodes_;
 };
 
 // persistent sentinel throughout all sentinel tests
-class SentinelMessageSimulation : public testing::Test,
-                                  public std::enable_shared_from_this<SentinelMessageSimulation> {
+class SentinelTest : public testing::Test {
  public:
-  SentinelMessageSimulation()
+  SentinelTest()
     : sentinel_([this](Address address) { SendGetClientKey(address); },
                 [this](GroupAddress group_address) { SendGetGroupKey(group_address); }) {}
 
   // Add a correct, cooperative group of indictated total size and responsive quorum
   void AddCorrectGroup(GroupAddress group_address, size_t group_size,
                        size_t active_quorum);
+
+  template <typename TypeData>
+  void SimulateMessage(GroupAddress)
 
   void SendGetClientKey(const Address node_address);
   void SendGetGroupKey(const GroupAddress group_address);
@@ -72,11 +83,29 @@ class SentinelMessageSimulation : public testing::Test,
   std::vector<SignatureGroup> groups_;
 };
 
-void SentinelMessageSimulation::SendGetClientKey(Address /*node_address*/) {
+void SentinelTest::AddCorrectGroup(GroupAddress group_address,
+                                                size_t group_size,
+                                                size_t active_quorum) {
+  auto itr = std::find_if(std::begin(groups_), std::end(groups_),
+                          [group_address](SignatureGroup group_)
+                          { return group_.Address() == group_address; });
+
+  if (itr == std::end(groups_))
+      groups_.push_back(SignatureGroup(group_address, group_size, active_quorum));
+  else assert("Sentinel test AddCorrectGroup: Group already exists");
+
+
+  static_cast<void>(itr);
+  static_cast<void>(group_address);
+  static_cast<void>(group_size);
+  static_cast<void>(active_quorum);
+}
+
+void SentinelTest::SendGetClientKey(Address /*node_address*/) {
 
 }
 
-void SentinelMessageSimulation::SendGetGroupKey(GroupAddress group_address) {
+void SentinelTest::SendGetGroupKey(GroupAddress group_address) {
   //std::lock_guard<std::mutex> lock(mutex_);
   auto itr = std::find_if(std::begin(groups_), std::end(groups_),
                           [group_address](SignatureGroup group_)
@@ -86,14 +115,17 @@ void SentinelMessageSimulation::SendGetGroupKey(GroupAddress group_address) {
 
 // first try for specific message type, generalise later
 // PutData is chosen as fundamental type with data payload.
-TEST(RoutingTest, BEH_SentinelSimpleAdd) {
+TEST_F(SentinelTest, BEH_SentinelSimpleAdd) {
 
   const auto serialised_data(RandomString(100));
   PutData put_message(DataTagValue::kImmutableDataValue,
                       std::vector<byte>(serialised_data.begin(),
                                         serialised_data.end()));
 
-
+  // Full group will respond correctly to Sentinel requests
+  const auto group_address(GroupAddress(NodeId(RandomString(NodeId::kSize))));
+  AddCorrectGroup(group_address, GroupSize, GroupSize);
+  SimulateMessage
 }
 
 }  // namespacce test
