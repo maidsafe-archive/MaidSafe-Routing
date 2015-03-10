cc8afd237af27c517ee3685fc158155d1597ab9e
mmoadeli <mmoadeli@maidsafe.net>
2015-03-10 22:55:57 +0000

    little further work on sentinel test

--- src/maidsafe/routing/tests/sentinel_test.cc
+++ src/maidsafe/routing/tests/sentinel_test.cc
@@ -24,6 +24,8 @@
 #include "maidsafe/common/utils.h"
 #include "maidsafe/common/make_unique.h"
 
+#include "maidsafe/passport/types.h"
+
 #include "maidsafe/routing/sentinel.h"
 #include "maidsafe/routing/messages/messages.h"
 
@@ -36,49 +38,52 @@ namespace test {
 class SentinelTest : public testing::Test {
  public:
   SentinelTest() : sentinel_(new Sentinel([](Address) {}, [](GroupAddress) {})) {}
+  
+  struct SentinelAddInfo {
+    MessageHeader header;
+    MessageTypeTag tag;
+    SerialisedMessage serialised;
+  };
 
+  template <typename MessageType>
+  SentinelAddInfo MakeAddInfo(const MessageType& message, const asymm::PrivateKey& private_key,
+                              DestinationAddress destination, SourceAddress source,
+                              MessageId message_id, Authority our_authority,
+                              MessageTypeTag tag) {
+    SentinelAddInfo add_info;
+    add_info.tag = tag;
+    add_info.serialised = Serialise(message);
+    add_info.header = MessageHeader(destination, source, message_id, our_authority,
+                                    asymm::Sign(add_info.serialised, private_key));
+    return add_info;
+  }
+  
+  void CreateMaidKeys(size_t quantity) {
+    while (quantity-- > 0)
+      maid_nodes_.emplace_back(passport::CreateMaidAndSigner().first);
+  }
+  
+  SentinelAddInfo CreateGerKeyResponse(const passport::Maid& maid, MessageId message_id) {
+    
+    return MakeAddInfo(<#const MessageType &message#>, <#const asymm::PrivateKey &private_key#>, <#DestinationAddress destination#>, <#maidsafe::routing::SourceAddress source#>, <#MessageId message_id#>, <#maidsafe::routing::Authority our_authority#>, <#maidsafe::routing::MessageTypeTag tag#>)
+  }
+  
  protected:
   std::unique_ptr<Sentinel> sentinel_;
+  std::vector<passport::Maid> maid_nodes_;
 };
-
-struct SentinelAddInfo {
-  MessageHeader header;
-  MessageTypeTag tag;
-  SerialisedMessage serialised;
-};
-
-template <typename MessageType>
-SentinelAddInfo MakeAddInfo(const MessageType& message, const asymm::Keys& keys,
-                            DestinationAddress destination, SourceAddress source,
-                            MessageId message_id, Authority our_authority,
-                            MessageTypeTag tag) {
-  SentinelAddInfo add_info;
-  add_info.tag = tag;
-  add_info.serialised = Serialise(message);
-  add_info.header = MessageHeader(destination, source, message_id, our_authority,
-                                  asymm::Sign(add_info.serialised, keys.private_key));
-  return add_info;
-}
-
-std::vector<asymm::Keys> CreateKeys(size_t quantity) {
- std::vector<asymm::Keys> key_pairs;
-  while (quantity-- > 0)
-    key_pairs.emplace_back(asymm::GenerateKeyPair());
-
-  return key_pairs;
-}
 
 TEST_F(SentinelTest, BEH_BasicNonGroupAdd) {
-  auto key_pairs(CreateKeys(1));
+  CreateMaidKeys(1);
   ImmutableData data(NonEmptyString(RandomString(NodeId::kSize)));
   auto serialised_data_string(data.Serialise().data.string());
   PutData put_data(DataTagValue::kImmutableDataValue, SerialisedData(serialised_data_string.begin(),
                                                                      serialised_data_string.end()));
-  auto add_info(MakeAddInfo(put_data, key_pairs.at(0),
+  auto add_info(MakeAddInfo(put_data, maid_nodes_.at(0).private_key(),
                             DestinationAddress(
                                 std::make_pair(Destination(NodeId(RandomString(NodeId::kSize))),
                                                boost::none)),
-                            SourceAddress(NodeAddress(NodeId(RandomString(NodeId::kSize))),
+                            SourceAddress(NodeAddress(NodeId(maid_nodes_.at(0).name()->string())),
                                           boost::none, boost::none),
                             MessageId(RandomUint32()), Authority::client_manager,
                             MessageTypeTag::PutData));
@@ -86,6 +91,7 @@ TEST_F(SentinelTest, BEH_BasicNonGroupAdd) {
   auto resolved(this->sentinel_->Add(add_info.header, add_info.tag, add_info.serialised));
   if (resolved)
     EXPECT_TRUE(false);
+  
 }
 
 }  // namespace test
